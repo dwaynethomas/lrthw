3ceee820bb51b817aae69a4b75995fa30cbc0f56
Dwayne503 <mdneue@yahoo.com>
2013-07-15 07:40:02 -0700

    Exercise 9

---
+++ lrthw.rb
@@ -0,0 +1,14 @@
+#Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJull\nAug"
+
+puts "Here are the days:", days
+puts "Here are the months:", months
+
+puts <<PARAGRAGH
+There's something going on here.
+With the PARAGRAGH thing
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAGH
