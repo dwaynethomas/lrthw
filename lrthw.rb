8ce280de9e0fdd2f09a873116488ad29b2ff2e4f
Dwayne503 <mdneue@yahoo.com>
2013-07-16 07:52:38 -0700

    Exercise 11 now included

--- lrthw.rb
+++ lrthw.rb
@@ -1,4 +1,4 @@
-tabby_cat = "\tI'm tabbed in."
+=begin tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
@@ -12,4 +12,18 @@ MY_HEREDOC
 puts tabby_cat
 puts persian_cat
 puts backslash_cat
-puts fat_cat
+puts fat_cat 
+=end
+
+#####Exercise 11
+print "How old are you?"
+age = gets.chomp
+print "How tall are you?"
+height = gets.chomp
+print "How much do you weigh?"
+weight = gets.chomp
+
+puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+
+
+ 
