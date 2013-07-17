1c5aae35dcca570722a5be7dc780b4306db59d21
Dwayne503 <mdneue@yahoo.com>
2013-07-16 21:28:08 -0700

    Exercise 12
    Exercise 12 introduces require features/ library and reading lines of
    text.

--- lrthw.rb
+++ lrthw.rb
@@ -13,7 +13,7 @@ puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat 
-=end
+
 
 #####Exercise 11
 print "How old are you?"
@@ -24,6 +24,18 @@ print "How much do you weigh?"
 weight = gets.chomp
 
 puts "So, you're #{age} years old, #{height} tall and #{weight} heavy."
+=end
 
+#Exercise 12
 
+require 'open-uri'
+
+open("http://www.ruby-lang.org/en") do |f|
+     f.each_line {|line| p line}
+     puts f.base_uri # <URI::http:0x40e6ef2 URL:http://www.ruby-lang.org/en/> #
+     puts f.content_type # "text/html"
+     puts f.charset # "iso-8859-1
+     puts f.content_encoding # []
+     puts f.last_modified # Thu Dec  05  02:245:02 UTC 2002
+end
  
