829ee913a5d4d6efbb089de877e9d05df9a8ad24
Dwayne503 <mdneue@yahoo.com>
2013-07-19 07:59:24 -0700

    Exercise 16
    Opening, Reading, Writing, Truncating,  and Closing files

--- lrthw.rb
+++ lrthw.rb
@@ -99,15 +99,15 @@ Alright, #{user}, so you said #{likes} about liking me.
 You live in #{lives}. Not sure where that is.
 And you have a #{computer}. Nice.
 MESSAGE
-=end
 
 #Exercise 15
-
+#accepts the filename with the scriptname
 filename = ARGV.first
 
 prompt = "> "
 txt = File.open(filename)
 
+ #Repeats the file information to the user and displays the content of the file
 puts "Here's your file: #{filename}"
 puts txt.read()
 
@@ -118,3 +118,43 @@ file_again = STDIN.gets.chomp()
 txt_again = File.open(file_again)
 
 puts txt_again.read()
+=end
+
+#Exercise 16
+
+filename = ARGV.first
+script = $0
+
+puts "We're going to erase #{filename}."
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+
+print "?"
+STDIN.gets
+
+puts "Opening the file..."
+target = File.open(filename, 'w')
+
+puts "Truncating the file. Goodbye!"
+target.truncate(target.size)
+
+
+puts "Now I'm going to ask you for three lines."
+
+print "line 1: "; line1 = STDIN.gets.chomp()
+print "line 2: "; line2 = STDIN.gets.chomp()
+print "line 3: "; line3 = STDIN.gets.chomp()
+
+puts "I'm going to write these to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+puts "And finally, we close it."
+target.close()
+
+
